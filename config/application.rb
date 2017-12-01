8812458533a4b96499d2e108354bb2a5150bf372
梅垣玄 <umegakigen@umegakigennoMacBook-Pro.local>
2017-12-01 11:33:13 +0900

    api added

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module TestProject
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.paths.add "app/api", glob: "**/*.rb"
+    config.autoload_paths += %W(#{config.root}/app/api)
+
+    config.middleware.use(Rack::Config) do |env|
+      env['api.tilt.root'] = Rails.root.join 'app', 'views', 'api'
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount API => "/api"
   resources :carts
   devise_for :users
 	root to: "test#index"
 	get "/items", to: "item#index"
 	post "/itemDetail", to: "item#itemDetail"
+
 end
